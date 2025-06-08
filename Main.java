e32717fedb46ba43075a85033001b6c3b658bd0e
@zhoukzhun <zhou8246@gmail.com>
2025-06-08 02:46:39 -0300

    Criar / Fazer Classe Pai "Produto" - Zhou
    Criar / Fazer Classe Pai "Produto" - Zhou

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         TelaPrincipal tela = new TelaPrincipal();
         tela.setVisible(true);
+
     }
 }
--- Produto.java
+++ Produto.java
@@ -1,5 +1,40 @@
 import java.io.Serializable;
 
 public abstract class Produto implements Serializable {
+    private String nomeProduto;
+    private String validadeProduto;
+    private float precoProduto;
+
+    public Produto(String nomeProduto, String validadeProduto, float precoProduto) {
+        this.nomeProduto = nomeProduto;
+        this.validadeProduto = validadeProduto;
+        this.precoProduto = precoProduto;
+    }
+
+    abstract void exibirInformacao();
+
+    public String getNomeProduto() {
+        return this.nomeProduto;
+    }
+
+    public String getValidadeProduto() {
+        return this.validadeProduto;
+    }
+
+    public float getPrecoProduto() {
+        return this.precoProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public void setValidadeProduto(String validadeProduto) {
+        this.validadeProduto = validadeProduto;
+    }
+
+    public void setPrecoProduto(float precoProduto) {
+        this.precoProduto = precoProduto;
+    }
 
 }
